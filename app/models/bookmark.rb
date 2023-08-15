cea731659606f91354e76699e1c1e0534e98cfce
analopezrdz <annalucable@gmail.com>
2023-08-15 14:38:24 +0200

    validations and references

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,7 @@
 class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :movie, :list, presence: true
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'has already been bookmarked in this list' }
+  validates :comment, length: { minimum: 6 }
 end
