b74cac9b83b5955f3ba2dcccd0d0ebbbb3f58034
analopezrdz <annalucable@gmail.com>
2023-08-17 14:34:23 +0200

    backgrpun lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, uniqueness: true
-  validates :name, :comment, :img_url, presence: true
+  validates :name, :img_url, presence: true
 end
